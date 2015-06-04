91a3cb0450cb92cc4cb7a0f2dc5543f160718fcb
David Sheehy <ds@schwey.io>
2015-06-04 16:33:31 -0500

    New url + view for existing list. The functional test passes.

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,10 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'items': items})
+    context_dict = {
+       'list':list_,
+    }
+    return render(request, 'list.html', context_dict)
 
 def new_list(request):
     list_ = List.objects.create()
